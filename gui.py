c0998e83ef0662a0fb117fe94843beab61add143
cchrissmm <chris@escapeecampers.com>
2023-12-13 08:30:43 +1100

    works with firefox session

--- gui.py
+++ gui.py
@@ -1,11 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import tkinter as tk
-import subprocess
-import os
 
 def open_next_link():
-    global current_link_index
-    vlc_path = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"  # Update this line
-
+    global current_link_index, driver
     try:
         with open("links.txt", "r") as file:
             links = file.readlines()
@@ -13,26 +11,38 @@ def open_next_link():
                 print("The links file is empty.")
                 return
 
+            if current_link_index >= len(links):
+                current_link_index = 0  # Loop back to the start
+
             url = links[current_link_index].strip()
-            print(f"Opening link: {url}")
 
-            try:
-                # Reduced caching time for quicker start
-                subprocess.Popen([vlc_path, "--network-caching=300", "--one-instance", "--play-and-exit", url])
-            except Exception as e:
-                print("Error opening VLC:", e)
+            # Open the link in the browser
+            driver.get(url)
 
-            current_link_index = (current_link_index + 1) % len(links)
+            # Increment the index for the next link
+            current_link_index += 1
 
     except FileNotFoundError:
         print("The links file was not found.")
 
+# Set Firefox options to enable autoplay
+firefox_options = Options()
+firefox_options.set_preference("media.autoplay.default", 0)  # 0 means "Allow all", 1 means "Block all"
+firefox_options.set_preference("media.autoplay.enabled.user-gestures-needed", False)
+firefox_options.set_preference("media.autoplay.block-webaudio", False)
+
+# Initialize the browser driver with the custom options
+driver = webdriver.Firefox(options=firefox_options)
+
 current_link_index = 0
 
 root = tk.Tk()
-root.title("Link Opener in VLC")
+root.title("YouTube Player")
 
-open_link_button = tk.Button(root, text="Open Next Link in VLC", command=open_next_link)
+open_link_button = tk.Button(root, text="Play Next Video", command=open_next_link)
 open_link_button.pack(pady=20)
 
 root.mainloop()
+
+# Close the browser when the GUI is closed
+driver.quit()
